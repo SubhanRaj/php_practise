a7b16a20a143b37b81ae387d3bba3d8489870be6
Subhan raj <shubhanraj2002@gmail.com>
2021-12-11 12:51:10 +0530

    variables in PHP

---
+++ basics/variables.php
@@ -0,0 +1,24 @@
+<?php
+
+// variables in PHP
+echo "Varaibles in PHP<br>";
+
+// variables are containers that store values
+// A variable name starts with a $ sign
+// A variable name is case-sensitive
+// A variable name must start with a letter or an underscore
+// A variable name can only contain letters, numbers, or underscores
+// Variable names are not allowed to start with a number
+
+$name = "Subhan";
+$age = "18";
+
+echo "$name is $age years old<br>";
+echo "$name is Doing Btech in CSE<br>";
+// Variables are case sensitive ig.
+
+$Name = "bro";
+
+echo "$name is my $Name";
+
+// here $name is a variable and $Name are diffrent variables
