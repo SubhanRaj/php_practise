81fffebc4cab66672fbb20dfecf483de28226402
Subhan raj <shubhanraj2002@gmail.com>
2021-12-11 10:56:27 +0530

    Data types

---
+++ basics/data_types.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Data Types in PHP</title>
+</head>
+
+<body>
+    <div class="nav justify-content-center">
+        
+        <h1>PHP Data Types</h1>
+    </div>
+    <h2>
+        PHP support the following data types:
+    </h2>
+    <ul>
+        <li>
+            <b>String</b>
+        </li>
+        <li>
+            <b>Integer</b>
+        </li>
+        <li>
+            <b>Float</b>
+        </li>
+        <li>
+            <b>Boolean</b>
+        </li>
+        <li>
+            <b>Array</b>
+        </li>
+        <li>
+            <b>Object</b>
+        </li>
+        <li>
+            <b>NULL</b>
+        </li>
+        <li>
+            <b>Resource</b>
+        </li>
+    </ul>
+    <?php
+    // String
+
+    $string = "This is a string";
+
+
+
+    ?>
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+
+</body>
+
+</html>
