5333dbd574f0c91388cd9ef62080d4babcf97475
Subhan raj <shubhanraj2002@gmail.com>
2021-12-26 19:44:51 +0530

    Data Types in PHP

--- basics/data_types.php
+++ basics/data_types.php
@@ -14,7 +14,7 @@
 
 <body>
     <div class="nav justify-content-center">
-        
+
         <h1>PHP Data Types</h1>
     </div>
     <h2>
@@ -50,6 +50,46 @@
     // String
 
     $string = "This is a string";
+    echo $string;
+    echo "<br>";
+
+    // Variable
+    // In PHP, a variable is a container for a value, which can be changed at any time.
+    // it can be a string, integer, float, boolean, array, object, NULL or resource.
+
+    $name = "Subhan";
+    $age = 20;
+    $university = "Khwaja Moinuddin Chishti Language University";
+
+    // Let's try to create a string that has above variables
+    $info = "Hi, My name is $name, I'm $age years old and I study at $university";
+    echo $info;
+
+    // Integer
+    // Integers are whole numbers, positive or negative.
+
+    $salary = 100000;
+    echo " <br> $name is $age years old and astonishingly he earns $salary";
+
+    // Float
+    // Float can store floating point numbers i.e. Decimal numbers.
+
+    $height = 151.52;
+    $weight = 54.5678;
+    echo "<br> $name is $height cm tall and weighs $weight kg";
+
+    // Boolean
+    // Boolean is a data type that can only have two values: true or false.
+
+    $true = true;
+    $false = false;
+    echo "<br> $name is $true and $name is $false";
+
+    // Array
+    // Arrays is a collection of similar data types which can store multiple valeus, they are accessed by using an index number.
+    // In PHP arrays are denoted by square brackets [], and individual values within the array are separated by commas.
+
+    $my_friends = array("Huzaifa, Jainendra, Pratyush");
 
 
 
