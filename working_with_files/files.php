ec63e7aa68060747313edc184fa7c570fbd5667a
SubhanRaj <shubhanraj2002@gmail.com>
2022-01-19 11:44:06 +0530

    basic File I/O in PHP

---
+++ working_with_files/files.php
@@ -0,0 +1,16 @@
+<?PHP
+
+// $read = readfile("note.txt");
+// echo $read;
+
+// This also prints number of chracters read in the file
+
+// So we go with following approach
+
+readfile("note.txt");
+echo "<br>";
+
+readfile("file.html");
+
+
+?>
